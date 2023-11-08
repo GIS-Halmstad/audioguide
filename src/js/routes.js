61d995f3edbe6b9643807764ced2b676f69ebdf7
Jacob Wodzyński <jacob.wod@gmail.com>
2023-11-08 10:34:44 +0100

    Removed one of the two pages, as it for now looks like we'll be good with one page with multiple views.

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,14 +1,9 @@
 import HomePage from "../pages/home.jsx";
-import GuidesPage from "../pages/guides.jsx";
 
 const routes = [
   {
     path: "/",
     component: HomePage,
-  },
-  {
-    path: "/guides",
-    component: GuidesPage,
   },
 ];
 
