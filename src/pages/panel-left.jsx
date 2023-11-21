757779a7f68ae759b1c953e54683e0c5d7e954a1
Jacob Wodzyński <jacob.wod@gmail.com>
2023-11-21 13:09:57 +0100

    Implemented the 'Show all guides' functionality.

--- src/pages/panel-left.jsx
+++ src/pages/panel-left.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-import { List, ListItem, Navbar, Page } from "framework7-react";
+import { List, ListItem, Navbar, Page, f7 } from "framework7-react";
+import store from "../js/store";
+import { updateFeaturesInMap } from "../js/olMap";
 
 function PanelLeft() {
+  const handleShowAllGuides = async () => {
+    // Tell OL to deselect any features
+    f7.emit("olFeatureSelected", []);
+
+    // Tell the Store to select all categories
+    await store.dispatch("setSelectedCategories", store.state.allCategories);
+
+    // Tell OL to update the map view
+    updateFeaturesInMap();
+
+    f7.panel.close("left");
+  };
+
   return (
     <Page>
       <Navbar title="Meny" />
@@ -9,7 +24,7 @@ function PanelLeft() {
         <ListItem
           title="Alla guider"
           panelClose
-          onClick={(e) => console.log(e)}
+          onClick={handleShowAllGuides}
         />
         <ListItem title="Jämfört kartor" />
         <ListItem title="Skriv ut" />
