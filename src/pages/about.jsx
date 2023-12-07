bbba2accf6e75942bde38ff1364f8656b004c1fb
Jacob Wodzyński <jacob.wod@gmail.com>
2023-12-07 15:16:22 +0100

    Added a button to show some technical debug info from the About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { f7, Page, Navbar, Block } from "framework7-react";
+import React, { useRef } from "react";
+import { f7, Page, Navbar, Block, Button } from "framework7-react";
 
 import { getOLMap } from "../js/olMap";
 
 const About = () => {
+  const popup = useRef(null);
   const audioguideLayersAttribution =
     f7.store.state.mapConfig.tools.audioguide.audioguideLayersAttribution;
   const aboutPageContent =
@@ -25,6 +26,47 @@ const About = () => {
       <p key={i}>{paragraph}</p> // Wrap each paragraph into a P element and render.
     ));
 
+  const createTechnicalPopup = () => {
+    console.log(f7.store.state);
+    const deviceDetection = Object.entries(f7.device)
+      .filter((a) => typeof a[1] !== "function")
+      .map((a) => `<li>${a[0]}: ${a[1]}</li>`)
+      .join("");
+
+    // Create popup
+    if (!popup.current) {
+      popup.current = f7.popup.create({
+        content: `
+          <div class="popup">
+            <div class="page">
+              <div class="navbar">
+                <div class="navbar-inner">
+                  <div class="navbar-bg"></div>
+                  <div class="title">Teknisk information</div>
+                  <div class="right"><a  class="link popup-close">Stäng</a></div>
+                </div>
+              </div>
+              <div class="page-content">
+                <div class="block" style="font-family: monospace;">
+                  <ul>
+                    ${deviceDetection}
+                  </ul>
+                  <hr />
+                  <ul>
+                    <li>mapServiceBase: ${f7.store.state.appConfig.mapServiceBase}</li>
+                    <li>mapName: ${f7.store.state.appConfig.mapName}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        `.trim(),
+      });
+    }
+    // Open it
+    popup.current.open();
+  };
+
   return (
     <Page>
       <Navbar title="Om Audioguide" backLink="Tillbaka" />
@@ -35,8 +77,12 @@ const About = () => {
         <Block className="page-content">{aboutPageParagraphs}</Block>
         <div
           style={{ fontSize: "x-small" }}
-          className={"margin-left margin-right"}
+          className={"margin-left margin-right margin-bottom"}
         >
+          <Button small onClick={createTechnicalPopup}>
+            Visa teknisk information
+          </Button>
+          <hr />
           <p>
             &copy; 2023-{new Date().getFullYear()} {audioguideLayersAttribution}
           </p>
