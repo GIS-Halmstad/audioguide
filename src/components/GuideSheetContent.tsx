bda48e42325631de51e41339359e31e84d5eb4c3
Jacob Wodzyński <jacob.wod@gmail.com>
2024-04-02 13:27:26 +0200

    Added non-breaking space in guide headings

--- src/components/GuideSheetContent.tsx
+++ src/components/GuideSheetContent.tsx
@@ -167,13 +167,17 @@ function GuideSheetContent({ activeGuideObject }) {
               flexDirection: "column",
               flex: 1,
               alignItems: "center",
+              textAlign: "center",
             }}
           >
-            <b>{`${activeGuideObject.line.get(
-              "title"
-            )} (${activeStopNumber} av ${
-              Object.entries(activeGuideObject.points).length
-            })`}</b>
+            <b>
+              {`${activeGuideObject.line.get(
+                "title"
+              )} (${activeStopNumber}\u00A0av\u00A0${
+                // \u00A0 is a non-breaking space: we want "x of y" to appear on the same line
+                Object.entries(activeGuideObject.points).length
+              })`}
+            </b>
             <span className="padding-left-half padding-right-half">
               {title}
             </span>
