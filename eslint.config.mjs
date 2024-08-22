d57a9be3d8ec054239cea31090e6650f8c51712e
Jacob Wodzyński <jacob.wod@gmail.com>
2024-08-22 09:53:33 +0200

    REVERT ME IN THE FUTURE: This commit disables react-hooks, as the eslint-plugin-react-hooks version currently available is incompatible with ESlint 9. Once the compatibility issues are resolved upstream, this commit can be reverted and react-hooks once again enabled in ESlint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+//import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
@@ -10,35 +10,40 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-)), {
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended"
+    )
+  ),
+  {
     plugins: {
-        react: fixupPluginRules(react),
-        "react-hooks": fixupPluginRules(reactHooks),
+      react: fixupPluginRules(react),
+      //"react-hooks": fixupPluginRules(reactHooks),
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        ecmaVersion: 2022,
-        sourceType: "module",
+      ecmaVersion: 2022,
+      sourceType: "module",
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
 
     rules: {},
-}];
+  },
+];
